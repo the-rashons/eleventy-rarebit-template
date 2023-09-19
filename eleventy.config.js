b663ad16a3e4ccdf4896b36bb1462d044568e01b
Leslie Swan <contact@personmeetup.ca>
2023-09-18 21:35:06 -0600

    Primed files for commenting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
+/**
+ * Person: This is the 11ty config!
+ */
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function(eleventyConfig) {
-    // Copy `img/` and `css/` folders to output
-    eleventyConfig.addPassthroughCopy("img");
-    eleventyConfig.addPassthroughCopy("css");
+		// Copy `img/` and `css/` folders to output
+		eleventyConfig.addPassthroughCopy("img");
+		eleventyConfig.addPassthroughCopy("css");
 }
