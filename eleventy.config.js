4a4d975078dc74fc79c233c51aaf88d3ac4ebe47
Leslie Swan <contact@personmeetup.ca>
2023-09-20 21:47:13 -0600

    Boilerplate remaining comments

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,14 @@
 /**
- * Person: This is the 11ty config!
+ * This is the 11ty config! If you ever need to get underneath the hood of 11ty
+ * to add functionality or to sort your collections, this would be the place to
+ * do it! In this example however, we just copy over the `img` and `css`
+ * folders over to the output.
+ * (https://www.11ty.dev/docs/config/)
  */
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function(eleventyConfig) {
-		// Copy `img/` and `css/` folders to output
+		// Copy `img` and `css` folders to output
 		eleventyConfig.addPassthroughCopy("img");
 		eleventyConfig.addPassthroughCopy("css");
 }
